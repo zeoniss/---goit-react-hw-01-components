14b854a0eadd7038728e97912282ee13c80dd06e
Vladimir Kriukov <zeoniss28@gmail.com>
2021-03-27 18:23:13 +0200

    summary

---
+++ src/components/Profile/Stats.js
@@ -0,0 +1,21 @@
+import React from 'react'
+const Stats = () => { <
+    ul class = "stats" >
+        <
+        li >
+        <
+        span class = "label" > Followers < /span> <
+        span class = "quantity" > 1000 < /span> <
+        /li> <
+        li >
+        <
+        span class = "label" > Views < /span> <
+        span class = "quantity" > 2000 < /span> <
+        /li> <
+        li >
+        <
+        span class = "label" > Likes < /span> <
+        span class = "quantity" > 3000 < /span> <
+        /li> <
+        /ul>
+}
